6834dd7396a5c2638a0503e164d816eeb8a7dc96
brit666 <appneutrino@gmail.com>
2026-01-04 09:43:38 +0600

    Idk what the fuck i am doing

---
+++ Client/View/ContentBrowserView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lumentra.View
+{
+    /// <summary>
+    /// Interaction logic for ContentBrowserView.xaml
+    /// </summary>
+    public partial class ContentBrowserView : Window
+    {
+        public ContentBrowserView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void WinClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void WinMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void WinMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+
+                maximize_btn.Content = "\xE923";
+            }
+            else if(this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+
+                maximize_btn.Content = "\xE922";
+            }
+        }
+
+    }
+}
--- Client/View/LoginView.xaml.cs
+++ Client/View/LoginView.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Shapes;
 
 namespace lumentra.View
 {
-    /// <summary>
-    /// Interaction logic for LoginView.xaml
-    /// </summary>
     public partial class LoginView : Window
     {
         public LoginView()
